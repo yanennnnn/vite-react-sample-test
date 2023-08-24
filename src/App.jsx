88448268e70797d6d1b30ff9de94fd3cae97699c
yanenn <sh5ojkmt@gmail.com>
2023-08-24 13:55:50 +0800

    fix: home 路徑部屬錯誤

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
-import Week2 from './pages/Week2'
+import Week2 from './pages/week2'
 function App() {
   return (
     <>
       <Routes>
-        <Route path={'/'} element={<Home />} /> 
+        <Route path={process.env.NODE_ENV === 'production' ? '/vite-react-sample-test/' : '/'} element={<Home />} /> 
         <Route path='week2' element={<Week2 />} />
       </Routes>
     </>
